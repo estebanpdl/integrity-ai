b68c2b41b8d266abdddc0307f69dd8667e1490d6
estebanpdl <pdlr.estebanfelipe@gmail.com>
2025-06-01 14:03:23 -0500

    chore: renamed key thread vars

--- moderation/openai_moderation.py
+++ moderation/openai_moderation.py
@@ -30,18 +30,18 @@ class OpenAIModeration:
 
         # get model limits
         self.model_limits = self.get_model_limits()
-        self.max_requests_per_min = self.model_limits['rpm']
-        self.max_tokens_per_min = self.model_limits['tpm']
-        self.max_requests_per_day = self.model_limits['rpd']
+        self.mod_max_requests_per_min = self.model_limits['rpm']
+        self.mod_max_tokens_per_min = self.model_limits['tpm']
+        self.mod_max_requests_per_day = self.model_limits['rpd']
 
         # shared threading locks
-        self.rate_limit_lock = threading.Lock()
-        self.token_lock = threading.Lock()
+        self.mod_rate_limit_lock = threading.Lock()
+        self.mod_token_lock = threading.Lock()
 
         # shared controls for rate limiting
-        self.request_timestamps = []
-        self.token_usage_log = []
-        self.daily_requests = 0
+        self.mod_request_timestamps = []
+        self.mod_token_usage_log = []
+        self.mod_daily_requests = 0
 
         # shared threading event
         self.stop_flag = threading.Event()
@@ -66,15 +66,7 @@ class OpenAIModeration:
         
         return model_limits['openai_moderation']['omni-moderation-latest']
     
-    def temp_log(self, message: str) -> None:
-        '''
-        Temporary log.
-        '''
-        path = './logs/openai_moderation.log'
-        with open(path, 'a', encoding='utf-8') as f:
-            f.write(f'{message}\n')
-    
-    def _enforce_rate_limits(self, completion_tokens: int) -> None:
+    def _mod_enforce_rate_limits(self, completion_tokens: int) -> None:
         '''
         Enforce rate limits for the OpenAI API.
         This method ensures that the number of requests and tokens used
@@ -88,38 +80,31 @@ class OpenAIModeration:
         '''
         while True:
             wait_time = 0
-            with self.rate_limit_lock, self.token_lock:
+            with self.mod_rate_limit_lock, self.mod_token_lock:
                 now = time.time()
 
-                self.temp_log(f'\n\n')
-
                 # timestamps and tokens used
-                self.request_timestamps[:] = [
-                    t for t in self.request_timestamps if now - t < 60.0
+                self.mod_request_timestamps[:] = [
+                    t for t in self.mod_request_timestamps if now - t < 60.0
                 ]
-                self.temp_log(f'request_timestamps: {self.request_timestamps}')
-                self.token_usage_log[:] = [
-                    (t, tokens) for t, tokens in self.token_usage_log if now - t < 60.0
+                self.mod_token_usage_log[:] = [
+                    (t, tokens) for t, tokens in self.mod_token_usage_log if now - t < 60.0
                 ]
-                self.temp_log(f'token_usage_log: {self.token_usage_log}')
-
-                if len(self.request_timestamps) >= self.max_requests_per_min:
-                    if self.request_timestamps:
-                        oldest_request_time = self.request_timestamps[0]
+                if len(self.mod_request_timestamps) >= self.mod_max_requests_per_min:
+                    if self.mod_request_timestamps:
+                        oldest_request_time = self.mod_request_timestamps[0]
                         request_wait = 60.0 - (now - oldest_request_time)
                     else:
                         request_wait = 1.0
                     wait_time = max(wait_time, request_wait)
                 
-                tokens_used = sum(tokens for _, tokens in self.token_usage_log)
-                self.temp_log(f'tokens_used: {tokens_used}')
+                tokens_used = sum(tokens for _, tokens in self.mod_token_usage_log)
 
                 # aggregated tokens
                 agg_tokens = tokens_used + completion_tokens
-                self.temp_log(f'agg_tokens: {agg_tokens}')
-                if agg_tokens > self.max_tokens_per_min:
-                    if self.token_usage_log:
-                        oldest_token_time = self.token_usage_log[0][0]
+                if agg_tokens > self.mod_max_tokens_per_min:
+                    if self.mod_token_usage_log:
+                        oldest_token_time = self.mod_token_usage_log[0][0]
                         token_wait = 60.0 - (now - oldest_token_time)
                     else:
                         token_wait = 1.0
@@ -128,12 +113,10 @@ class OpenAIModeration:
                 if wait_time == 0:
                     # no enforced wait time
                     now = time.time()
-                    self.request_timestamps.append(now)
-                    self.temp_log(f'request_timestamps added: {self.request_timestamps}')
+                    self.mod_request_timestamps.append(now)
 
                     # increment daily requests
-                    self.daily_requests += 1
-                    self.temp_log(f'daily_requests: {self.daily_requests}')
+                    self.mod_daily_requests += 1
 
                     # add jitter to avoid thread pileup
                     jitter = self.DEFAULT_JITTER + random.uniform(0, 0.05)
@@ -160,21 +143,24 @@ class OpenAIModeration:
         :rtype: dict
         '''
         # enforce rate limits
-        self._enforce_rate_limits(completion_tokens)
+        self._mod_enforce_rate_limits(completion_tokens)
 
         # make request
-        if self.daily_requests < self.max_requests_per_day:
+        if self.mod_daily_requests < self.mod_max_requests_per_day:
             response = self.client.moderations.create(
                 model=self.model,
                 input=content
             )
 
             # update token usage log
-            self.token_usage_log.append(
+            self.mod_token_usage_log.append(
                 (time.time(), completion_tokens)
             )
 
             # return response
-            return response.model_dump()
+            return {
+                'model': self.model,
+                'results': response.model_dump()['results'][0]
+            }
         else:
             raise Exception('Daily request limit reached')
