40a624c474a69b0378ba1c63c1f6ae5c4bb22bc9
estebanpdl <pdlr.estebanfelipe@gmail.com>
2025-06-02 17:25:57 -0500

    chore: incorporated new classes

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,5 @@ from .model import LanguageModel
 from .openai_client import OpenAIGPT
 from .groq_client import GroqModels
 from .claude_client import ClaudeModels
+from .gemini_client import GeminiModels
+from .xai_client import XAIGrok
